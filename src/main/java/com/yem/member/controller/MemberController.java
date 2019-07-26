6846d9405002047072857a15106d94277e906de4
guchuang <hbszguchuang@163.com>
2019-07-26 17:28:18 +0800

    stae 2

--- src/main/java/com/yem/member/controller/MemberController.java
+++ src/main/java/com/yem/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import com.yem.common.BaseMsgResponse;
 import com.yem.constant.ApiConstant;
 import com.yem.constant.Constants;
 import com.yem.dto.YemMemberDTO;
@@ -19,7 +20,6 @@ import com.yem.request.ModifyYemMemberRequest;
 import com.yem.request.QueryYemMemberListRequest;
 import com.yem.request.QueryYemMemberRequest;
 import com.yem.response.AddYemMemberResponse;
-import com.yem.response.BaseMsgResponse;
 import com.yem.response.ModifyYemMemberResponse;
 import com.yem.response.QueryYemMemberListResponse;
 import com.yem.response.QueryYemMemberResponse;
